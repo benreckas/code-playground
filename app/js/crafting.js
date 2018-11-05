981f71c7f758d4f9cbf8457da78e1749f84d94ba
benreckas <benreckas@gmail.com>
2018-11-05 09:09:10 -0800

    feat(various) Init commit

---
+++ app/js/crafting.js
@@ -0,0 +1,166 @@
+const trapper = [
+  {
+    name: '',
+    quantity: 0
+  },
+];
+
+const satchels = [
+  {
+    name: 'Badger Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Beaver Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Bison Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Boar Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Buck Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Cougar Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Deer Pelt',
+    quantity: 7
+  },
+  {
+    name: 'Elk Pelt',
+    quantity: 2
+  },
+  {
+    name: 'Iguana Skin',
+    quantity: 1
+  },
+  {
+    name: 'Panther Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Rabbit Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Raccoon Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Squirrel Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Wolf Pelt',
+    quantity: 1
+  }
+];
+
+const saddles = [
+
+]
+
+const camp = [
+  {
+    name: 'Alligator Skin',
+    quantity: 1
+  },
+  {
+    name: 'Beaver Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Bison Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Boar Pelt',
+    quantity: 5
+  },
+  {
+    name: 'Buck Carcass',
+    quantity: 1
+  },
+  {
+    name: 'Buck Antlers',
+    quantity: 1
+  },
+  {
+    name: 'Cougar Pelt',
+    quantity: 2
+  },
+  {
+    name: 'Coyote Pelt',
+    quantity: 2
+  },
+  {
+    name: 'Elk Antlers',
+    quantity: 1
+  },
+  {
+    name: 'Fox Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Goat Hide',
+    quantity: 2
+  },
+  {
+    name: 'Moose Antler',
+    quantity: 1
+  },
+  {
+    name: 'Muskrat Pelt',
+    quantity: 1
+  },
+  {
+    name: 'Ox Hide',
+    quantity: 2
+  },
+  {
+    name: 'Pronghorn Hide',
+    quantity: 2
+  },
+  {
+    name: 'Pronghorn Carcass',
+    quantity: 1
+  },
+  {
+    name: 'Ram Carcass',
+    quantity: 1
+  },
+  {
+    name: 'Snake Skin',
+    quantity: 2
+  },
+  {
+    name: 'Wolf Pelt',
+    quantity: 2
+  },
+  {
+    name: 'Wolf Carcass',
+    quantity: 1
+  }
+];
+
+const legendaryAnimals = [
+  {
+    name: 'Legendary Bear',
+    location: 'Blah',
+    completed: true,
+    image: `bear.jpg`
+  },
+  {
+    name: 'Legendary Bear',
+    location: 'Blah',
+    completed: true
+  },
+]
